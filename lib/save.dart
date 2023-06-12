252303c1dd20416e89561b44d76a8ba431fed0a9
ElijahOd <124442472+ElijahOd@users.noreply.github.com>
2023-06-12 16:46:47 +0300

    Update save.dart

--- lib/save.dart
+++ lib/save.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
 
+class SavesPage extends StatelessWidget {
+  const SavesPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        title: const Text(
+          'Saved GIFs',
+          style: TextStyle(
+            color: Color(0xff6b6b6b),
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: const Center(
+        child: Text('List of saved GIFs goes here'),
+      ),
+    );
+  }
+}
