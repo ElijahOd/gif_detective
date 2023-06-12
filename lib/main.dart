dc4bc463edfdfbc3eaf3157e38db8ab02ed38abc
ElijahOd <124442472+ElijahOd@users.noreply.github.com>
2023-06-12 17:54:20 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class _MyAppState extends State<MyApp> {
             ),
             BottomNavigationBarItem(
               icon: Icon(Icons.save),
-              label: 'Save',
+              label: 'Saved',
             ),
           ],
         ),
